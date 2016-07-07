cef7e373dd451ba27d7e5de3656f3b0dc03cce78
Edwin de Jonge <edwindjonge@gmail.com>
2016-07-07 16:19:51 +0200

    first commit

---
+++ R/treecolors.R
@@ -0,0 +1,8 @@
+#' Create color palette based on hierarchical structure
+#'
+#' Create color palette based on hierarchical structure
+#' @export
+#' @param ... not used
+treecolors <- function(...){
+
+}
